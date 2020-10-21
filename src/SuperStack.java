263eff9ac7dd669dd1d6e338eba74458c9e1c40a
{ikemnkur-lab} <{ikem.c.nkurumeh-1@ou.edu}>
2020-10-21 15:45:08 -0600

    added SuperStack.java

---
+++ src/SuperStack.java
@@ -0,0 +1,26 @@
+
+public class SuperStack<T> {
+
+	private T[] items;
+	private int top;
+	
+	public SuperStack<T> (int size) {
+		items = (T[])new Object[size];
+	}
+	
+	public boolean isEmpty() {
+		return true;
+	}
+	
+	public boolean isFull() {
+		return true;
+	}
+	
+	public boolean push (T item) {
+		return true;
+	}
+	
+	public T pop() {
+		
+	}
+}
